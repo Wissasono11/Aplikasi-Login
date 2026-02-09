83af2174bce6bbb1d4901dbc66d17a8155a8aa56
Bayu Wicaksono <143940265+Wissasono11@users.noreply.github.com>
2026-02-09 13:04:30 +0700

    Enhance comments and documentation in login-script.js
    Added detailed comments and documentation for variables and event handling in the login script.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,23 +1,56 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+/** @module Login-Script */
+/**
+ *  Make variabel loginFormElement for login form.
+ *  @constant {HTMLElement}
  */
-
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+ *  Make variabel inputEmailElement for email input.
+ *  @constant {HTMLElement}
+ */
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+ *  Make variabel inputPasswordElement for password input.
+ *  @constant {HTMLElement}
+ */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+ *  Make variabel expectedEmail for current email.
+ *  @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+// add event listeer to login form
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
+
+    /**
+     *  variabel email for data email from button when clicked.
+     * @constant {string}
      */
+    const email = inputEmailElement.value;
+
+    /**
+     *  variabel password for data password from button when clicked.
+     * @constant {string}
+     */
+    const password = inputPasswordElement.value; 
+  
+    // Check condition if email and password correct
+    if (email === expectedEmail && password === expectedPassword){
+
+      // If password correct direct into function goToHome
+      goToHome();
+
+    } else {
+
+      // If password incorrect direct into function showPopUp()
+      showPopUp();
+      
+    }
 });
